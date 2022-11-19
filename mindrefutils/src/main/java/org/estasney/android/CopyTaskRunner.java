7b3a52ec83e3e87b69909d65fb79c7dffb0640d3
estasney <estasney@users.noreply.github.com>
2022-11-18 19:12:16 -0500

    complete writeFileToExternal
    Add check for files existing in app storage but not external storage. If found, remove them.

--- mindrefutils/src/main/java/org/estasney/android/CopyTaskRunner.java
+++ mindrefutils/src/main/java/org/estasney/android/CopyTaskRunner.java
@@ -4,22 +4,18 @@ import static org.estasney.android.MindRefFileUtils.combinePath;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 import android.content.ContentResolver;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.ParcelFileDescriptor;
-import android.provider.DocumentsContract;
 import android.util.Log;
 
-import androidx.annotation.NonNull;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayDeque;
+import java.util.stream.Stream;
 
 public class CopyTaskRunner {
 
@@ -54,37 +50,53 @@ public class CopyTaskRunner {
     public static void mirrorDirectory(Uri sourceFolderUri, File targetDir, ContentResolver contentResolver) throws IOException {
 
         MindRefFileData[] fileData = MindRefFileData.getChildrenFromUri(sourceFolderUri, contentResolver);
+        // Gather targetDir Children - if not present in sourceFolder, they are deleted
+        ArrayDeque<Path> targetDirPathDeque = new ArrayDeque<>();
+        Stream<Path> targetDirFiles = Files.list(targetDir.toPath()).filter(t -> t.toFile().isFile());
+        targetDirFiles.forEach(targetDirPathDeque::add);
+
+
         for (MindRefFileData srcChild : fileData) {
             if (srcChild.isDirectory) {
                 Path targetChildDir = combinePath(targetDir.getPath(), srcChild.displayName);
                 mirrorDirectory(srcChild.uri, targetChildDir.toFile(), contentResolver);
             } else {
                 Path targetChild = combinePath(targetDir.toString(), srcChild.displayName);
-                mirrorFile(srcChild, targetChild, contentResolver);
+                mirrorFile(srcChild, targetChild.toFile(), contentResolver);
+                targetDirPathDeque.remove(targetChild);
             }
         }
 
+        // Remove any children
+        while (!targetDirPathDeque.isEmpty()) {
+            Path hangingChildPath = targetDirPathDeque.pop();
+            Log.d(TAG, "Removing: " + hangingChildPath);
+            Files.delete(hangingChildPath);
+        }
+
+
     }
+
+    /**
+     * Given a file from App Storage, Persist it to External Storage using DocumentProvider
+     * If the file does not exist in External Storage, it will be created.
+     * @param sourcePath - Location of the app file
+     * @param name - Name of the file, without suffix
+     * @param mimeType - MimeType of sourcefile
+    * @param externalDir - Category folder to save in External Storage
+     * @throws IOException - Thrown when the category does not exist
+     */
 
     public static void writeFileToExternal(Path sourcePath, String name, String mimeType, MindRefFileData externalDir,
                                            ContentResolver contentResolver) throws IOException {
         // We need a URI for a category so we query the root
-        Uri targetUri;
-        try {
-            targetUri = DocumentsContract.createDocument(contentResolver, externalDir.uri, mimeType, name);
-        } catch (FileNotFoundException e) {
-            Log.w(TAG, "Failed to create new document at " + externalDir.uri, e);
-            return;
-        }
-        if (targetUri == null) {
-            Log.w(TAG, "Failed to create new document at " + externalDir.uri + " with name: " + name );
-        }
+        MindRefFileData externalTarget = externalDir.getOrMakeChild(contentResolver, name, mimeType);
 
         // Read source document
         byte[] srcData = Files.readAllBytes(sourcePath);
 
         // Now we have URI, open file descriptor and copy
-        ParcelFileDescriptor pfd = contentResolver.openFileDescriptor(targetUri, "w");
+        ParcelFileDescriptor pfd = contentResolver.openFileDescriptor(externalTarget.uri, "w");
         FileOutputStream fileOutputStream = new FileOutputStream(pfd.getFileDescriptor());
         fileOutputStream.write(srcData);
         fileOutputStream.close();
